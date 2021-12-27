8259192851f8456cf509a9f3871bf2b381894145
Rick Mark <rickmark@outlook.com>
2021-12-27 00:30:20 -0800

    Sorting nvram

--- _tools/sort_yaml.rb
+++ _tools/sort_yaml.rb
@@ -1,9 +1,12 @@
 # 
 
 require 'yaml'
+require 'active_support/all'
 
-FILE_NAME = ARGV[1]
+FILE_NAME = ARGV[0]
 
 dictionary = YAML.load_file FILE_NAME
 
-dictionary.
+result = dictionary.sort_by { |item| item[:name] }
+
+File.open(FILE_NAME, "w") { |file| file.write(result.to_yaml) }
