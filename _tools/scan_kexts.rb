a3f0b1ae9e01d023cc9c066a0a52a6634c05b181
Rick Mark <rickmark@outlook.com>
2022-01-11 17:09:26 -0800

    Sorted kexts

--- _tools/scan_kexts.rb
+++ _tools/scan_kexts.rb
@@ -8,20 +8,34 @@ require 'active_support/all'
 require 'yaml'
 require 'macho'
 
+KERNEL_COLLECTION_DIR = '/System/Library/KernelCollections'
+
 KEXT_DATA_FILE = File.join(File.dirname(__FILE__), '../_data/kext.yaml')
 
 result = YAML.load_file KEXT_DATA_FILE
 
-result ||= {}
+result ||= []
 
-puts 'Usage: scan_kexts.rb <KEXT_COLLECTION>'
+entries = {}
 
-file = MachO.open ARGV[0]
+result.each { |entry| entries[entry['id']] = entry }
 
-binary = file.is_a?(MachO::FatFile) ? file.fat_archs[0] : file
+Dir.glob(File.join(KERNEL_COLLECTION_DIR, '*.kc')).each do |collection|
+  collection_file = File.basename collection
+  file = MachO.open collection
 
-binary.command(:LC_FILESET_ENTRY).each do |command|
-  result[command.entry_id.to_s] ||= { 'description' => nil }
+  binary = file.is_a?(MachO::FatFile) ? file.fat_archs[0] : file
+
+  binary.command(:LC_FILESET_ENTRY).each do |command|
+    bundle_id = command.entry_id.to_s
+    entries[bundle_id] ||= { 'id' => bundle_id, 'description' => nil }
+    collection = entries[bundle_id]['collections'] ||= []
+    collection << collection_file unless collection.include? collection_file
+  end
 end
+
+result = entries.values
+
+result.sort_by! { |entry| entry['id'] }
 
 File.write(KEXT_DATA_FILE, result.to_yaml)
