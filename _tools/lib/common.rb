f3b87dc1c16569787fb662edee4856266d4d88f0
Rick Mark <rickmark@outlook.com>
2022-04-16 17:00:53 -0700

    Final set of rake migrations and datafile collection API

--- _tools/lib/common.rb
+++ _tools/lib/common.rb
@@ -23,10 +23,28 @@ DATA_DIR = File.realdirpath File.join(File.dirname(__FILE__), '..', '..', '_data
 
 SCHEMAS_DIR = File.join(File.dirname(__FILE__), '..', '_schema')
 
+# Represents a DataFile element collection, which is a hash of uniquely identified entries with a hash for a value.
+# The hash will contain 'description' as a first element for human annotation
+class DataFileCollection
+  def initialize(data_file, collection_name)
+    data_file.data[collection_name.to_s] ||= {}
+    @collection_data = data_file.data[collection_name.to_s]
+  end
+
+  def ensure_key(key)
+    value = @collection_data[key.to_s] ||= {}
+    value['description'] ||= nil
+    value
+  end
+end
+
 # Base class for all data files
 class DataFile
+  attr_reader :data
+
   def initialize(*parts)
     @parts = parts
+    @collections = {}
     load_file(*parts)
     ensure_metadata
   end
@@ -38,16 +56,27 @@ class DataFile
     @data = YAML.load_file @filename if File.exist? @filename
   end
 
-  def ensure_metadata; end
+  def save!
+    save_data data
+  end
+
+  def save
+    save!
+  end
+
+  def collection(name)
+    @collections[name.to_s] ||= DataFileCollection.new(self, name)
+  end
+
+  private
 
   def save_data(data)
     File.write(@filename, data.to_yaml)
   end
 
-  attr_reader :data
-
-  def save!
-    save_data data
+  def ensure_metadata
+    @data['metadata'] ||= {}
+    @data['metadata'].reverse_merge!({ 'description' => nil, 'credits' => [] })
   end
 end
 
