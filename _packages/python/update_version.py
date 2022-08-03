843d45b48de4f8dd88943d674c2053f3b36a70c0
Rick Mark <rickmark@outlook.com>
2022-08-03 10:42:23 -0700

    Fix version replacement

--- _packages/python/update_version.py
+++ _packages/python/update_version.py
@@ -11,7 +11,7 @@ project_content = None
 with open('pyproject.toml', 'r') as input_file:
     project_content = input_file.read()
 
-project_content = project_content.replace('1.0.0', f"1.0.{height}")
+project_content = project_content.replace('version = "1.0.0"', f"version = \"1.0.{height}\"")
 
 with open('pyproject.toml', 'w') as output_file:
     output_file.write(project_content)
