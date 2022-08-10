2e978730eab67abcaf542ca638515f3e55065563
Rick Mark <rickmark@outlook.com>
2022-08-10 16:08:35 -0700

    Py url (#54)
    * Fix version gen and main file
    
    * Add live url methods

--- _packages/python/tests/test_smoke.py
+++ _packages/python/tests/test_smoke.py
@@ -4,3 +4,12 @@ import apple_data
 def test_smoke_test():
     data = apple_data.load_file('cores')
     assert data
+
+
+def test_url_naked():
+    url = apple_data.get_url('cores')
+    assert url == 'https://docs.hackdiff.rent/.data/cores.json'
+
+def test_url_yaml():
+    url = apple_data.get_url('cores', 'yaml')
+    assert url == 'https://docs.hackdiff.rent/.data/cores.yaml'
