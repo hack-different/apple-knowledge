275126ed496771eede0c8816299aa467e9be50eb
Rick Mark <rickmark@outlook.com>
2022-08-25 22:31:30 -0700

    Package udpate2 (#63)
    * Device tree parsing
    
    * Device tree parsing
    
    * Updated packages
    
    * Python support for binary tags

--- _packages/python/build.py
+++ _packages/python/build.py
@@ -5,6 +5,19 @@ import shutil
 import yaml
 import json
 
+
+def binary_constructor(loader: yaml.SafeLoader, node: yaml.nodes.MappingNode) -> str:
+    """Construct an employee."""
+    return str(node.value)
+
+
+def get_loader():
+    """Add constructors to PyYAML loader."""
+    loader = yaml.SafeLoader
+    loader.add_constructor("!binary", binary_constructor)
+    return loader
+
+
 def get_repo_height() -> int:
     git_branch_height = subprocess.run(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE)
 
@@ -19,15 +32,19 @@ def build(kwargs):
             os.mkdir(output_data)
 
         for file in input_data.rglob('**/*.yaml'):
-            input_file = pathlib.Path(file).relative_to(input_data)
-            output_file = output_data.joinpath(input_file.with_suffix('.json'))
+            try:
+                input_file = pathlib.Path(file).relative_to(input_data)
+                output_file = output_data.joinpath(input_file.with_suffix('.json'))
 
-            print(f"Copying {input_file} to {output_file}")
-            # Read YAML file
-            with open(file, 'r') as stream:
-                data_loaded = yaml.safe_load(stream)
+                print(f"Copying {input_file} to {output_file}")
+                # Read YAML file
+                with open(file, 'r') as stream:
+                    data_loaded = yaml.load(stream, Loader=get_loader())
 
-                output_file.parent.mkdir(parents=True, exist_ok=True)
+                    output_file.parent.mkdir(parents=True, exist_ok=True)
 
-                with open(output_file, 'w') as outfile:
-                    json.dump(data_loaded, outfile, indent=4)
+                    with open(output_file, 'w') as outfile:
+                        json.dump(data_loaded, outfile, indent=4)
+            except Exception as e:
+                print(f"Error handling {file}:\n\n{e}")
+                raise e
