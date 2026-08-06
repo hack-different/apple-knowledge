d0497fd36cc107ca2e6001a211ec77a4c8160ce8
Rick Mark <rickmark@outlook.com>
2026-08-05 22:01:33 -0700

    PKI OID map

--- _packages/ruby/lib/apple_data/dsl/data_file_definition.rb
+++ _packages/ruby/lib/apple_data/dsl/data_file_definition.rb
@@ -9,13 +9,21 @@ module AppleData
         @methods = {}
       end
 
-      def collection(name, mapper = nil, &)
-        @collections[name] = AppleData::DSL::CollectionDefinition.new(name, mapper)
-        if block_given? && @collections[name].mapper.nil?
-          @collections[name].mapper = Class.new(AppleData::SchemaBase)
-          @collections[name].mapper.class_eval(&)
+      def collection(collection_name, mapper = nil, &)
+        data_collection_name = self.class.name
+        @collections[collection_name] = AppleData::DSL::CollectionDefinition.new(collection_name, mapper)
+        if block_given? && @collections[collection_name].mapper.nil?
+          @collections[collection_name].mapper = Class.new(AppleData::SchemaBase)
+          @collections[collection_name].mapper.const_set(:DATA_COLLECTION_NAME, data_collection_name)
+          @collections[collection_name].mapper.const_set(:COLLECTION_NAME, collection_name)
+          @collections[collection_name].mapper.instance_eval do
+            def name
+              "#{self.class.const_get(:DATA_COLLECTION_NAME)}:#{self.class.const_get(:COLLECTION_NAME)}"
+            end
+          end
+          @collections[collection_name].mapper.class_eval(&)
         end
-        @collections[name]
+        @collections[collection_name]
       end
 
       def define_method(name, &proc)
---
+++ spec/apple_data/schemas/pki_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe AppleData::Schemas::PKI do
+  let(:instance) { described_class.new }
+
+  it 'has entries' do
+    aggregate_failures do
+      expect(instance.oids).to be_a(Hash)
+      expect(instance.oids).not_to be_empty
+    end
+  end
+
+  it 'has entries that respond to #to_s' do
+    aggregate_failures do
+      expect(instance.oids[:'1.2.840.113635.100.6.1.15'].to_s).to be_a(String)
+      expect(instance.oids[:'1.2.840.113635.100.6.1.15'].to_s).to eq 'appleImg4Manifest'
+    end
+  end
+end
